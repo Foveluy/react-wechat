67c79965f66caaaaeb61047318d16d90f8316123
Z F <215566435@qq.com>
2018-04-27 16:38:38 +1200

    chore: demo_page

--- build/pages/index/index.js
+++ build/pages/index/index.js
@@ -1,15 +1,5 @@
-const i = 'shit';
-const react_wechat_obj = {
-  data: {
-    foo: 100
-  },
-  onLoad: function () {},
-  onTaps: function () {
-    this.setData({
-      foo: this.data.foo + 10
-    });
-  }
-}
+
+const react_wechat_obj = {}
 const onLoad = react_wechat_obj.onLoad
 react_wechat_obj.onLoad = function(args){
     onLoad.call(this,args)
--- temple/pages/index/index.js
+++ temple/pages/index/index.js
@@ -1,39 +1,18 @@
 import { Page } from '../../wechat'
-import Modal from '../../components/modal/modal'
-const i = 'shit'
 
-function Second() {
-  return (
-    <view>
-      <view> {foo} </view>
-      <view> second无状态组件 </view>
-    </view>
-  )
-}
 export default class Index extends Page {
-  state = {
-    foo: 100
-  }
   window = {
-    navigationBarTitleText: 'react-wechat'
+    navigationBarTitleText: 'React Wechat',
+    navigationBarBackgroundColor: '#282c34',
+    navigationBarTextStyle: 'white'
   }
 
-  onLoad() {}
-
-  onTaps() {
-    this.setState({
-      foo: this.state.foo + 10
-    })
-  }
   render() {
-    const { foo } = this.state
     return (
-      <view
-        class="container"
-        bindtap={this.onTaps}
-        style={{ height: `200px`, background: `rgba(${foo + 20},3,${foo + 20},1)` }}
-      >
+      <view class="container">
         <image class="logo" src="../../res/reaction.svg" />
+        <view class="title">你好，React 小程序</view>
+        <view class="sub-title">使用 React 写法书写小程序</view>
       </view>
     )
   }
