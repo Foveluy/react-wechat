32c77ce409b281d473bed660dc8d848983261b94
方正 <215566435@qq.com>
2018-04-27 22:07:24 +1200

    chore: stateless md

---
+++ docs/stateless.md
@@ -0,0 +1,24 @@
+# 小程序无状态组件
+
+在小程序中，无状态组件较为特殊，其使用方法和 react 无状态组件有所出入
+
+```jsx
+const First = ({ bar }) => {
+    return <view>{bar}</view>
+}
+//这是一个简单的无状态组件
+class Demo extends Page {
+    state = {
+        foo: 1
+    }
+
+    render() {
+        return (
+            <view>
+                <First data="{{foo}}" />
+                <First data="{{bar:'我是bar'}}" >
+            </view>
+        )
+    }
+}
+```
--- test/temple/page.js
+++ test/temple/page.js
@@ -1,44 +1,54 @@
 import First from './outside'
 
 function Second() {
-  return (
-    <view>
-      <view> {foo} </view>
-      <view> second无状态组件 </view>
-    </view>
-  )
-}
-export default class Index extends Page {
-  state = {
-    foo: 1
-  }
-  window = {
-    navigationBarTitleText: 'react-wechat'
-  }
-
-  onLoad() {}
-
-  check() {
-    const a = i + 'hahah'
-    this.setState({
-      foo: '213'
-    })
-  }
-  onTaps() {
-    this.setState({
-      foo: this.state.foo + 1
-    })
-  }
-  render() {
-    const { foo } = this.state
     return (
-      <view class="container">
-        <view style={{ height: `${foo}px`, width: `${foo + 1 - 3 - 4 + 2 + foo}px` }}>你好 react-wechat</view>
-        <view style={{ height: `${bar}px`, background: `rgba(${bar},3,3,1)` }}>你好 react-wechat</view>
-        <view style={{ height: '300px', background: `rgba(${foo + 100},3,3,1)` }} />
-        <First />
-        <Second />
-      </view>
+        <view>
+            <view> {foo} </view>
+            <view> second无状态组件 </view>
+        </view>
     )
-  }
+}
+const Third = () => {
+    return (
+        <view>
+            <view> {foo} </view>
+            <view> Third无状态组件 </view>
+        </view>
+    )
+}
+
+export default class Index extends Page {
+    state = {
+        foo: 1
+    }
+    window = {
+        navigationBarTitleText: 'react-wechat'
+    }
+
+    onLoad() {}
+
+    check() {
+        const a = i + 'hahah'
+        this.setState({
+            foo: '213'
+        })
+    }
+    onTaps() {
+        this.setState({
+            foo: this.state.foo + 1
+        })
+    }
+    render() {
+        const { foo } = this.state
+        return (
+            <view class="container">
+                <view style={{ height: `${foo}px`, width: `${foo + 1 - 3 - 4 + 2 + foo}px` }}>你好 react-wechat</view>
+                <view style={{ height: `${bar}px`, background: `rgba(${bar},3,3,1)` }}>你好 react-wechat</view>
+                <view style={{ height: '300px', background: `rgba(${foo + 100},3,3,1)` }} />
+                <First />
+                <Second />
+                <Third shit={foo} />
+            </view>
+        )
+    }
 }
