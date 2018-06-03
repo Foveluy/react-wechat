af3a11a37eb7ca8dfa1ddebbc188d2b4bf536af6
方正 <215566435@qq.com>
2018-06-03 20:53:59 +0800

    push

--- temple/pages/index/index.js
+++ temple/pages/index/index.js
@@ -1,26 +1,26 @@
 import { Page } from '../../wechat'
 
 function Button({ title }) {
-    return (
-        <view class="rw-button">
-            <view>{title}</view>
-        </view>
-    )
+  return (
+    <view class="rw-button">
+      <view>{title}</view>
+    </view>
+  )
 }
 export default class Index extends Page {
-    window = {
-        navigationBarTitleText: 'React Wechat',
-        navigationBarBackgroundColor: '#282c34',
-        navigationBarTextStyle: 'white'
-    }
-    render() {
-        return (
-            <view class="container">
-                <image class="logo" src="../../res/reaction.svg" />
-                <view class="title">你好，React 小程序</view>
-                <view class="sub-title">使用 React 写法书写小程序</view>
-                <Button data="{{title:'快速开始'}}" />
-            </view>
-        )
-    }
+  window = {
+    navigationBarTitleText: 'React Wechat',
+    navigationBarBackgroundColor: '#282c34',
+    navigationBarTextStyle: 'white'
+  }
+  render() {
+    return (
+      <view class="container">
+        <image class="logo" src="../../res/reaction.svg" />
+        <view class="title">你好，React 小程序</view>
+        <view class="sub-title">使用 React 写法书写小程序</view>
+        <Button data="{{title:'你好小程序'}}" />
+      </view>
+    )
+  }
 }
